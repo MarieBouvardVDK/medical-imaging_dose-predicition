d663e2a7bd620e4c749c1f51ebd17a2711411664
marie_bouvard <marie.bouvard@student-cs.fr>
2023-03-11 16:07:14 +0100

    modifed dataset

--- helpers/dataset.py
+++ helpers/dataset.py
@@ -26,3 +26,25 @@ class DoseDataset(torch.utils.data.Dataset):
             return concat_data, dose.unsqueeze(0)
         
         return concat_data
+
+
+class TestDataset(torch.utils.data.Dataset):
+    def __init__(self, data_path, transform=None):
+        self.data_path = data_path
+        self.samples = os.listdir(data_path)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        sample_path = self.data_path + os.sep + self.samples[idx]
+
+
+        ct_scan = torch.from_numpy(np.load(sample_path + os.sep + 'ct.npy')).float()
+        possible_dose_mask = torch.from_numpy(np.load(sample_path + os.sep + 'possible_dose_mask.npy')).float()
+        structure_masks = torch.from_numpy(np.load(sample_path + os.sep + 'structure_masks.npy')).float()
+      
+        concat_data = torch.cat((ct_scan.unsqueeze(0), possible_dose_mask.unsqueeze(0), structure_masks), 0)
+        
+        return concat_data, self.samples[idx]
