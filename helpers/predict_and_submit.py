6205d1a3ab6fea01c43d8c5a76b089f2bc9288aa
marie_bouvard <marie.bouvard@student-cs.fr>
2023-03-11 16:06:24 +0100

    created prediction and submission function

--- helpers/predict_and_submit.py
+++ helpers/predict_and_submit.py
@@ -1,6 +1,17 @@
 import numpy as np
 import torch
+import time
+import os
+import tqdm
 
-def make_predictions(model, test_dataloader):
-    predictions = model(test_dataloader)
+def predict_and_submit(model, test_dataloader, submission=True, save_path = '/Users/mariebouvard/Desktop/medical-imaging/submissions'):
+    print('Making predictions...')
+    for i, (image, name) in enumerate(tqdm(test_dataloader)):
+        start_predict = time.process_time()
+        predictions = model(image)
+        if submission == True: 
+            save_name = save_path + name[0]
+            np.save(save_name, predictions)
+    print(f'Predictions made. Took {time.process_time() - start_predict}s.')
+
     return predictions
