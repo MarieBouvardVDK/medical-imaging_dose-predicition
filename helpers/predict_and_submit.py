36943a4652850e2d0622806cca28a2c68585a2b5
amandineallmang <90215812+amandineallmang@users.noreply.github.com>
2023-03-12 10:09:01 +0100

    Updating function with cuda and cpu movement

--- helpers/predict_and_submit.py
+++ helpers/predict_and_submit.py
@@ -4,15 +4,30 @@ import time
 import os
 import tqdm
 
-def predict_and_submit(model, test_dataloader, submission=True, save_path = '/Users/mariebouvard/Desktop/medical-imaging/submissions'):
+def predict_and_submit(model, test_dataloader, submission=True, save_path = path_for_pred):
     print('Making predictions...')
+    #moving to GPU if possible
+    cuda = True if torch.cuda.is_available() else False
+    Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+    #setting model to evaluation mode
+    model.eval()
+    
     with torch.no_grad():
-        for i, (image, name) in enumerate(tqdm(test_dataloader)):
-            start_predict = time.process_time()
-            predictions = model(image)
-            if submission == True: 
-                save_name = save_path + name[0]
-                np.save(save_name, predictions)
+      #iterating over dataloader
+      for i, (image, name) in enumerate(tqdm(test_dataloader)):
+          #moving image to cuda
+          image = image.type(Tensor)
+          start_predict = time.process_time()
+          #making prediction
+          predictions = model(image)
+          #moving pred back to cpu
+          predictions = predictions.cpu()
+          #create submission file
+          if submission == True: 
+              save_name = save_path + name[0]
+              np.save(save_name, predictions)
+            
     print(f'Predictions made. Took {time.process_time() - start_predict}s.')
 
     return predictions
