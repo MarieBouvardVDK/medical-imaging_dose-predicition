19c8e299ce1f8ba2939467ef3b826fe1b32c698c
amandineallmang <90215812+amandineallmang@users.noreply.github.com>
2023-03-18 11:55:32 +0100

    Adding possibility of transform on ct_scan + comments

--- helpers/dataset.py
+++ helpers/dataset.py
@@ -3,6 +3,10 @@ import numpy as np
 import os
 
 class DoseDataset(torch.utils.data.Dataset):
+    '''
+    This class creates a DoseDataset to load the input data and real dose output.
+    DoseDatasets will be used for the train and validation data.
+    '''
     def __init__(self, data_path, transform=None):
         self.data_path = data_path
         self.samples = os.listdir(data_path)
@@ -13,20 +17,32 @@ class DoseDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         sample_path = self.data_path + os.sep + self.samples[idx]
-
+        
+        #input data: ct + possible dose + structured masks
         ct_scan = torch.from_numpy(np.load(sample_path + os.sep + 'ct.npy')).float()
         possible_dose_mask = torch.from_numpy(np.load(sample_path + os.sep + 'possible_dose_mask.npy')).float()
         structure_masks = torch.from_numpy(np.load(sample_path + os.sep + 'structure_masks.npy')).float()
-      
+        
+        #applying transform is required
+        if self.transform is not None:
+            ct_scan = self.transform(ct_scan)
+        
+        #combine all input data
         concat_data = torch.cat((ct_scan.unsqueeze(0), possible_dose_mask.unsqueeze(0), structure_masks), 0)
-
+        
+        #real dose 
         dose = torch.from_numpy(np.load(sample_path + os.sep + 'dose.npy'))
+        
         return concat_data, dose.unsqueeze(0)
         
     
 
 
 class TestDataset(torch.utils.data.Dataset):
+    '''
+    This class creates a TestDataset to load the input data and the corresponding file name.
+    The TestDataset will be used for the test data.
+    '''
     def __init__(self, data_path, transform=None):
         self.data_path = data_path
         self.samples = os.listdir(data_path)
@@ -38,11 +54,12 @@ class TestDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         sample_path = self.data_path + os.sep + self.samples[idx]
 
-
+        #input data: ct scan + possible dose + structure masks
         ct_scan = torch.from_numpy(np.load(sample_path + os.sep + 'ct.npy')).float()
         possible_dose_mask = torch.from_numpy(np.load(sample_path + os.sep + 'possible_dose_mask.npy')).float()
         structure_masks = torch.from_numpy(np.load(sample_path + os.sep + 'structure_masks.npy')).float()
-      
+        
+        #combine the input data
         concat_data = torch.cat((ct_scan.unsqueeze(0), possible_dose_mask.unsqueeze(0), structure_masks), 0)
         
         return concat_data, self.samples[idx]
